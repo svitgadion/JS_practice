6b4cc4cde0a2a3554ca92c97a5c7b2e5f1570889
Svitlana Gadion <svitlanagadion@gmail.com>
2024-12-02 23:09:10 +0200

    add js

---
+++ my-script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+console.log("JavaScript is awesome!");
+console.log(10);
+console.log("Jacob Mercer");
+console.log(26);
+
+const age = 20;
+console.log(age);
+
+const username = "Mango";
+console.log(username);
+
+const productName = "Droid";
+const pricePerItem = 2000;
+
+
